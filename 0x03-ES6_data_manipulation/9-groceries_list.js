a02a894af2eb7bd51ae77745c1e8224363f8b9f0
Salma-fathi <salma89040@gmail.com>
2024-10-08 22:07:44 +0400

    add task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+export default function getListStudents() {
+  const values = new Set([
+
+  ['Apples' , 10],
+  ['Tomatoes' , 10],
+  ['Pasta' , 1],
+  ['Rice' ,1],
+  ['Banana' ,5]
+  ]);
+  
+
+  return  new map (values);
+}
